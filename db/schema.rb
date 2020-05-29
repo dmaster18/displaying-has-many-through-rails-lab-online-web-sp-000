891644a6f50b5eb13bc527d29ab287c3967a5b8f
David Strauch <dmaster18@gmail.com>
2020-05-29 18:46:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200529174219) do
+ActiveRecord::Schema.define(version: 20200529174135) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
     t.integer  "doctor_id"
     t.integer  "patient_id"
-  end
-
-  create_table "doctors", force: :cascade do |t|
-    t.string "name"
-    t.string "department"
   end
 
 end
