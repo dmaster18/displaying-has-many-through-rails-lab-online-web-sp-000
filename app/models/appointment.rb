0267d3f7f15a626840972b37265a4b6a3e7165e7
David Strauch <dmaster18@gmail.com>
2020-05-29 19:04:28 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -37,33 +37,10 @@ class Appointment < ApplicationRecord
       month_name = "December"
     end
 
-    if hour == 13
-      hour = 1
-    elsif hour == 14
-      hour = 2
-    elsif hour == 15
-      hour = 3
-    elsif hour == 16
-      hour = 4
-    elsif hour == 17
-      hour = 5
-    elsif hour == 18
-      hour = 6
-    elsif hour == 19
-      hour = 7
-    elsif hour == 20
-      hour = 8
-    elsif hour == 21
-      hour = 9
-    elsif hour == 22
-      hour = 10
-    elsif hour == 23
-      hour = 11
-    end
 
     if min == 0
       min = "00"
-    end  
+    end
 
     "#{month_name} #{day}, #{year} at #{hour}:#{min}"
   end
