0446c6a7d3613391e8cbf8e5eabaaa8071a9bbb7
David Strauch <dmaster18@gmail.com>
2020-05-29 18:45:56 +0000

    Automatically backed up by Learn

--- db/migrate/20200529174135_create_appointments.rb
+++ db/migrate/20200529174135_create_appointments.rb
@@ -2,7 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.string :
+      t.string :convert_time
       t.integer :doctor_id
       t.integer :patient_id
     end
